0e662f012aa2bc465afd811a87a818c00f0b6261
vdpb20 <66538008+vdpb20@users.noreply.github.com>
2020-06-06 19:51:35 -0400

    Update TweetManager.py

--- GetOldTweets3/manager/TweetManager.py
+++ GetOldTweets3/manager/TweetManager.py
@@ -355,7 +355,7 @@ class TweetManager:
         try:
             response = opener.open(url)
             jsonResponse = response.read()
-            time.sleep(5)
+            time.sleep(20)
             except Exception as e:
             print("An error occured during an HTTP request:", str(e))
             print("Try to open in browser: https://twitter.com/search?q=%s&src=typd" % urllib.parse.quote(urlGetData))
